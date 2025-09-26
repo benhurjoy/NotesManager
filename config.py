5c84648dd7bac83fc1fdabe245c8e2b4cee52649
benhurjoy <benhurjoygundeti@gmail.com>
2025-09-26 11:10:37 +0530

    v7

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Load the .env file
+load_dotenv(dotenv_path="/home/yourusername/NotesManager/.env")
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-EMAIL_USER = os.getenv("EMAIL_USER")
-EMAIL_PASS = os.getenv("EMAIL_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = int(os.environ.get("DB_PORT", 3306))
+DB_NAME = os.environ.get("DB_NAME")
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+EMAIL_USER = os.environ.get("EMAIL_USER")
+EMAIL_PASS = os.environ.get("EMAIL_PASS")
